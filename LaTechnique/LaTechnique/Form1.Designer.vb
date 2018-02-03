e50fdef8e59a9d317197bfb353ebf8d7b0b0fec8
fr4nckk <frank1993.23@gmail.com>
2018-02-03 18:39:58 -0500

    Test de conflit pour Jojo
    Bonne chance!

--- LaTechnique/LaTechnique/Form1.Designer.vb
+++ LaTechnique/LaTechnique/Form1.Designer.vb
@@ -22,16 +22,30 @@ Partial Class Form1
     'Ne la modifiez pas à l'aide de l'éditeur de code.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.lblFrank = New System.Windows.Forms.Label()
         Me.SuspendLayout()
+        '
+        'lblFrank
+        '
+        Me.lblFrank.AutoSize = True
+        Me.lblFrank.Location = New System.Drawing.Point(193, 48)
+        Me.lblFrank.Name = "lblFrank"
+        Me.lblFrank.Size = New System.Drawing.Size(34, 13)
+        Me.lblFrank.TabIndex = 0
+        Me.lblFrank.Text = "Frank"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.ClientSize = New System.Drawing.Size(714, 437)
+        Me.Controls.Add(Me.lblFrank)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Private WithEvents lblFrank As Label
 End Class
