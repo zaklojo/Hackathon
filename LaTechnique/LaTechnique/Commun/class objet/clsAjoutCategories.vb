e08489e6d4b0f74c9f79dea75d2914b6c43564ba
BuildTools <unconfigured@null.spigotmc.org>
2018-02-04 00:59:59 -0500

    correction ajout categories
    claude

--- LaTechnique/LaTechnique/Commun/class objet/clsAjoutCategories.vb
+++ LaTechnique/LaTechnique/Commun/class objet/clsAjoutCategories.vb
@@ -1,34 +1,45 @@
-﻿Public Class clsAntecedentsVaccins
+﻿Public Class clsAjoutCategories
 
 
     'Variables
-    Private v_id_vaccin As Integer
-    Private v_assurance_maladie As Integer
+    Private v_categorie As Integer
+    Private v_assurance_maladie As String
+    Private v_description As String
 
     'Propriétés
-    Public Property id_vaccin As Integer
+    Public Property categorie As Integer
         Get
-            Return v_id_vaccin
+            Return v_categorie
         End Get
         Set(ByVal value As Integer)
-            v_id_vaccin = value
+            v_categorie = value
         End Set
     End Property
 
-    Public Property assurance_maladie As Integer
+    Public Property assurance_maladie As String
         Get
             Return v_assurance_maladie
         End Get
-        Set(ByVal value As Integer)
+        Set(ByVal value As String)
             v_assurance_maladie = value
+        End Set
+    End Property
+
+    Public Property description As String
+        Get
+            Return v_description
+        End Get
+        Set(ByVal value As String)
+            v_description = value
         End Set
     End Property
 
     'Mapping
     Public Overloads Sub mapping(ByVal dataAccess As dataAccessObjet, ligne As Integer)
         With dataAccess.ds
-            id_vaccin = verifierColonneDataSet(.Tables(0), ligne, constantes_antecedents_vaccins.STR_ID_ANTECEDENTS_VACCINS)
-            assurance_maladie = verifierColonneDataSet(.Tables(0), ligne, constantes_antecedents_vaccins.STR_ASSURANCE_MALADIE)
+            v_categorie = verifierColonneDataSet(.Tables(0), ligne, constantes_ajout_categories.STR_CATEGORIE)
+            assurance_maladie = verifierColonneDataSet(.Tables(0), ligne, constantes_ajout_categories.STR_ASSURANCE_MALADIE)
+            description = verifierColonneDataSet(.Tables(0), ligne, constantes_ajout_categories.STR_DESCRIPTION)
         End With
     End Sub
 
