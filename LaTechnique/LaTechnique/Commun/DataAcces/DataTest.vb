9aa8d8f94c47fc8d234df3e9c3442dcbdfac420a
fr4nckk <frank1993.23@gmail.com>
2018-02-04 02:19:06 -0500

    connection a la base de donnee

---
+++ LaTechnique/LaTechnique/Commun/DataAcces/DataTest.vb
@@ -0,0 +1,9 @@
+﻿Public Class DataTest
+    Inherits dataAccessObjet
+
+    Public Sub test()
+        bd.Requete($"select * from {constantes_personnes.STR_TABLE}", ds, da, constantes_personnes.STR_TABLE)
+    End Sub
+
+
+End Class
