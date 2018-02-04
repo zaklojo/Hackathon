cf0e170b945ef56f21c106d2baaae31d8ca94761
zaklojo <zaklojo@gmail.com>
2018-02-03 23:53:18 -0500

    Remove les interface brisées

--- LaTechnique/LaTechnique/Commun/Constantes/constantes_antecedants_psychiatriques.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class constantes_antecedants_psychiatriques
-
-    Public Const STR_TABLE As String = "antecedants_psychiatriques"
-
-
-    Public Const STR_ASSURANCE_MALADIE As String = "assurance_maladie"
-    Public Const STR_ID_ANTECEDANTS_PSYCHIATRIQUES As String = "id_psychiatrique"
-
-End Class
