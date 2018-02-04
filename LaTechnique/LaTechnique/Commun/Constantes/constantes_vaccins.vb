33ad86d347aeb35ddbb1368ca5c18aa107545712
BuildTools <unconfigured@null.spigotmc.org>
2018-02-03 20:39:20 -0500

    vaccins ne prend pas de s

--- LaTechnique/LaTechnique/Commun/Constantes/constantes_vaccins.vb
+++ LaTechnique/LaTechnique/Commun/Constantes/constantes_vaccins.vb
@@ -3,7 +3,7 @@
     Public Const STR_TABLE As String = "vaccins"
 
 
-    Public Const STR_ID_VACCIN As String = "id_vaccin"
+    Public Const STR_ID_VACCIN As String = "id_vaccins"
     Public Const STR_NOM As String = "nom"
 
 End Class
