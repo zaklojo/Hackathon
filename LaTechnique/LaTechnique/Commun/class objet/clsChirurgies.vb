a67727deef05f78b3ff7e45842b443311ef9fc71
zaklojo <zaklojo@gmail.com>
2018-02-04 00:52:27 -0500

    Fonction en master

--- LaTechnique/LaTechnique/Commun/class objet/clsChirurgies.vb
+++ LaTechnique/LaTechnique/Commun/class objet/clsChirurgies.vb
@@ -1,34 +1,34 @@
-﻿Public Class clsAntecedentsVaccins
+﻿Public Class clsChirurgies
 
 
     'Variables
-    Private v_id_vaccin As Integer
-    Private v_assurance_maladie As Integer
+    Private v_id_chirurgie As Integer
+    Private v_nom_Chirurgie As String
 
     'Propriétés
-    Public Property id_vaccin As Integer
+    Public Property id_chirurgie As Integer
         Get
-            Return v_id_vaccin
+            Return v_id_chirurgie
         End Get
         Set(ByVal value As Integer)
-            v_id_vaccin = value
+            v_id_chirurgie = value
         End Set
     End Property
 
-    Public Property assurance_maladie As Integer
+    Public Property nom_Chirurgie As String
         Get
-            Return v_assurance_maladie
+            Return v_nom_Chirurgie
         End Get
-        Set(ByVal value As Integer)
-            v_assurance_maladie = value
+        Set(ByVal value As String)
+            v_nom_Chirurgie = value
         End Set
     End Property
 
     'Mapping
     Public Overloads Sub mapping(ByVal dataAccess As dataAccessObjet, ligne As Integer)
         With dataAccess.ds
-            id_vaccin = verifierColonneDataSet(.Tables(0), ligne, constantes_antecedents_vaccins.STR_ID_ANTECEDENTS_VACCINS)
-            assurance_maladie = verifierColonneDataSet(.Tables(0), ligne, constantes_antecedents_vaccins.STR_ASSURANCE_MALADIE)
+            id_chirurgie = verifierColonneDataSet(.Tables(0), ligne, constantes_chirurgies.STR_ID_CHIRURGIE)
+            nom_Chirurgie = verifierColonneDataSet(.Tables(0), ligne, constantes_chirurgies.STR_NOM)
         End With
     End Sub
 
