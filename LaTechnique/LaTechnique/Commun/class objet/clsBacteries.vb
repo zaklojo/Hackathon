09b2a3aec8dc554c859e771d05492cc06ed46427
zaklojo <zaklojo@gmail.com>
2018-02-04 00:51:39 -0500

    FINAL constantes et object

--- LaTechnique/LaTechnique/Commun/class objet/clsBacteries.vb
+++ LaTechnique/LaTechnique/Commun/class objet/clsBacteries.vb
@@ -1,34 +1,34 @@
-﻿Public Class clsAntecedentsVaccins
+﻿Public Class clsBacteries
 
 
     'Variables
-    Private v_id_vaccin As Integer
-    Private v_assurance_maladie As Integer
+    Private v_id_bacterie As Integer
+    Private v_nom_bacteries As String
 
     'Propriétés
-    Public Property id_vaccin As Integer
+    Public Property id_bacterie As Integer
         Get
-            Return v_id_vaccin
+            Return v_id_bacterie
         End Get
         Set(ByVal value As Integer)
-            v_id_vaccin = value
+            v_id_bacterie = value
         End Set
     End Property
 
-    Public Property assurance_maladie As Integer
+    Public Property nom_bacteries As String
         Get
-            Return v_assurance_maladie
+            Return v_nom_bacteries
         End Get
-        Set(ByVal value As Integer)
-            v_assurance_maladie = value
+        Set(ByVal value As String)
+            v_nom_bacteries = value
         End Set
     End Property
 
     'Mapping
     Public Overloads Sub mapping(ByVal dataAccess As dataAccessObjet, ligne As Integer)
         With dataAccess.ds
-            id_vaccin = verifierColonneDataSet(.Tables(0), ligne, constantes_antecedents_vaccins.STR_ID_ANTECEDENTS_VACCINS)
-            assurance_maladie = verifierColonneDataSet(.Tables(0), ligne, constantes_antecedents_vaccins.STR_ASSURANCE_MALADIE)
+            id_bacterie = verifierColonneDataSet(.Tables(0), ligne, constantes_bacteries.STR_ID_BACTERIE)
+            nom_bacteries = verifierColonneDataSet(.Tables(0), ligne, constantes_bacteries.STR_NOM)
         End With
     End Sub
 
--- LaTechnique/LaTechnique/Commun/class objet/clsMaladieMentales.vb
+++ LaTechnique/LaTechnique/Commun/class objet/clsMaladieMentales.vb
@@ -1,34 +1,34 @@
-﻿Public Class clsAntecedentsVaccins
+﻿Public Class clsMaladieMentales
 
 
     'Variables
-    Private v_id_vaccin As Integer
-    Private v_assurance_maladie As Integer
+    Private v_id_psychiatrique As Integer
+    Private v_nom_maladie_mentale As String
 
     'Propriétés
-    Public Property id_vaccin As Integer
+    Public Property id_psychiatrique As Integer
         Get
-            Return v_id_vaccin
+            Return v_id_psychiatrique
         End Get
         Set(ByVal value As Integer)
-            v_id_vaccin = value
+            v_id_psychiatrique = value
         End Set
     End Property
 
-    Public Property assurance_maladie As Integer
+    Public Property nom_maladie_mentale As String
         Get
-            Return v_assurance_maladie
+            Return v_nom_maladie_mentale
         End Get
-        Set(ByVal value As Integer)
-            v_assurance_maladie = value
+        Set(ByVal value As String)
+            v_nom_maladie_mentale = value
         End Set
     End Property
 
     'Mapping
     Public Overloads Sub mapping(ByVal dataAccess As dataAccessObjet, ligne As Integer)
         With dataAccess.ds
-            id_vaccin = verifierColonneDataSet(.Tables(0), ligne, constantes_antecedents_vaccins.STR_ID_ANTECEDENTS_VACCINS)
-            assurance_maladie = verifierColonneDataSet(.Tables(0), ligne, constantes_antecedents_vaccins.STR_ASSURANCE_MALADIE)
+            id_psychiatrique = verifierColonneDataSet(.Tables(0), ligne, constantes_maladies_mentales.STR_ID_PSYCHIATRIQUE)
+            nom_maladie_mentale = verifierColonneDataSet(.Tables(0), ligne, constantes_maladies_mentales.STR_NOM)
         End With
     End Sub
 
