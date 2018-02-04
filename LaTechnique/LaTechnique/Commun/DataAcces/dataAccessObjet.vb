962f19f398aef8c0fe8588afebc218de9412be97
zaklojo <zaklojo@gmail.com>
2018-02-03 20:36:28 -0500

    ClassesObjets

---
+++ LaTechnique/LaTechnique/Commun/DataAcces/dataAccessObjet.vb
@@ -0,0 +1,3 @@
+﻿Public Class dataAccessObjet
+
+End Class
---
+++ LaTechnique/LaTechnique/Commun/class objet/clsAntecedantsPsychiatrique.vb
@@ -0,0 +1,35 @@
+﻿Public Class clsAntecedantsChirurgicaux
+
+
+    'Variables
+    Private v_id_allergie As Integer
+    Private v_assurance_maladie As Integer
+
+    'Propriétés
+    Public Property id_allergie As Integer
+        Get
+            Return v_id_allergie
+        End Get
+        Set(ByVal value As Integer)
+            v_id_allergie = value
+        End Set
+    End Property
+
+    Public Property assurance_maladie As Integer
+        Get
+            Return v_assurance_maladie
+        End Get
+        Set(ByVal value As Integer)
+            v_assurance_maladie = value
+        End Set
+    End Property
+
+    'Mapping
+    Public Overloads Sub mapping(ByVal dataAccess As dataAccessObjet, ligne As Integer)
+        With dataAccess.ds
+            id_allergie = verifierColonneDataSet(.Tables(0), ligne, constantes_antecedants_chirurgicaux.STR_ID_ANTECEDANTS_CHIRURGICAUX)
+            assurance_maladie = verifierColonneDataSet(.Tables(0), ligne, constantes_antecedants_chirurgicaux.STR_ASSURANCE_MALADIE)
+        End With
+    End Sub
+
+End Class
